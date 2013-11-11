19d789f9f5aa5122a906d129f488ec61bffa9778
Graeme Tait <burriko@gmail.com>
2013-11-11 13:53:55 +0000

    Add the user provider

---
+++ src/NclCareers/HardcodedAuthentication/HardcodedUserProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace NclCareers\HardcodedAuthentication;
+
+use Illuminate\Auth\UserProviderInterface,
+    Illuminate\Auth\UserInterface,
+    Illuminate\Auth\GenericUser,
+    Config;
+
+class HardcodedUserProvider implements UserProviderInterface
+{
+	public function retrieveByID($id)
+	{
+		if ($id !== 1)
+			return false;
+
+		return $this->createUser();
+	}
+
+	public function retrieveByCredentials(array $credentials)
+	{
+		if ($credentials['email'] !== $this->getUsername())
+			return false;
+
+		return $this->createUser();
+	}
+
+	public function validateCredentials(UserInterface $user, array $credentials)
+	{
+		return $this->isValidUser($credentials);
+	}
+
+	protected function getUsername()
+	{
+		$user = $this->fetchUser();
+		return $user['username'];
+	}
+
+	protected function getPassword()
+	{
+		$user = $this->fetchUser();
+		return $user['password'];
+	}
+
+	protected function fetchUser()
+	{
+		return Config::get('auth.hardcoded');
+	}
+
+	protected function createUser()
+	{
+		return new GenericUser(array(
+			'id'       => 1,
+			'username' => $this->getUsername()
+		));
+	}
+
+	protected function isValidUser($user)
+	{
+		return $user['email'] === $this->getUsername()
+		   and $user['password'] === $this->getPassword();
+	}
+}
